7c3116f6a594250220cefb3f25c515df67e01232
rawinolivera <oli18001@byui.edu>
2022-02-09 08:27:00 -0500

    update css last

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,10 +1,13 @@
+/* Last Modified */
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector('#current-year').textContent = year;
+document.querySelector('#last-mod').textContent = document.lastModified;
+
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
-function move(){
-    document.getElementById("head-date").style.display = "none";
+    document.getElementById("head-date").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn");

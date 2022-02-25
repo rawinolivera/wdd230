7bfdbb8e948480a450928eacaf7b5c51b4482124
rawinolivera <oli18001@byui.edu>
2022-02-25 09:11:41 -0500

    date update

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -14,20 +14,17 @@ const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
 /*CURRENT DATE*/ 
-// select the elements to manipulate (output to)
-const datefieldUK = document.querySelector("#head-date"); // for european/family history format with day first.
+const datefield = document.querySelector("#head-date");
+// for european/family history format with day first.
 
 // derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
 // long, medium, short options ... try them
 
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
 //Day of the Week
 const weekDay = new Date().getDay();

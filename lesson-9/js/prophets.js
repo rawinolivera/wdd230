2d1da2bcdb8e7971ed89c683d7fcd4009924b994
rawinolivera <oli18001@byui.edu>
2022-03-04 08:35:14 -0500

    working on lesson 9 - prophets

---
+++ lesson-9/js/prophets.js
@@ -0,0 +1,37 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)   
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+     //   prophets.forEach(displayProphets);
+    });
+
+const prophets = jsonObject['prophets'];
+//loop
+prophets.forEach(displayProphets);
+
+function displayProphets(prophet) {
+    let card = documnent.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+
+    //change the text context
+    h2.textContent = prophet.name + ' ' + prophet.lastname;
+    
+    //built image attributes
+    ____.setAttribute('srl', prophet.imageurl);
+    ____.setAttribute('alt', 'Portrait of' + prophet.name + ' ' + prophet.lastname);
+    ____.setAttribute('loading', 'lazy');
+
+    //add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(portrait);
+
+    //add/append the existing HTML div with the cards class with the section(card)
+    cards.appendChild(card);
+}
+

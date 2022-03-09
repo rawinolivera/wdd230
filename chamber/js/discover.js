df59bd18f0dbe00820c3586acf36bf951edc1bc7
rawinolivera <oli18001@byui.edu>
2022-03-08 23:16:46 -0500

    closing lesson7 chamber

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -37,27 +37,6 @@ function meetBanner() {
     }
 }
 
-//Calling the function!
-(meetBanner)();
-
-let t = parseFloat(document.querySelector("#t").textContent);
-let s = parseFloat(document.querySelector("#s").textContent);
-let windchill = "";
-
-if (t <= 50 && s > 3){
-    windchill = windChill(t, s);
-    windchill = `${windchill}&#176;F`;
-} else {
-    windchill = "N/A";
-} 
-
-//output
-document.querySelector("#w").innerHTML = windchill;
-
-function windChill(temp,speed) {
-    return Math.round(35.74 + .6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
-}
-
 //LAZY LOWDING IMAGES
 
 //getting images

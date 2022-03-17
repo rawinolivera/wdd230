0a635ab6324bdaba74162498bb2427264eba0716
rawinolivera <oli18001@byui.edu>
2022-03-17 13:46:04 -0500

    updating contact page

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -40,42 +40,4 @@ function meetBanner() {
 //Calling the function!
 (meetBanner)();
 
-//LAZY LOADING IMAGES
 
-//getting images
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-//optional parameter 
-const imgOptions = {
-    rootMargin: "0px 0px 50px 0px",
-    threshold: 1,
-};
-
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
-
-//first check to see if
-if('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    });
-
-    //loop through each image and check status load if necessary
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    });
-} else {
-    //just load ALL images if not supported
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-}

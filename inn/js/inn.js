b4270d46bcf88a12eb34e83cc142e9e49168e2a0
rawinolivera <oli18001@byui.edu>
2022-04-03 02:04:23 -0500

    updates

--- inn/js/inn.js
+++ inn/js/inn.js
@@ -2,10 +2,20 @@
 
 function toggleMenu() {
     document.querySelector(".menu").classList.toggle("open");
+    document.querySelector("#menuBtn").classList.toggle("open");
 }
 
+function toggleCall() {
+    document.querySelector("#phone").classList.toggle("come");
+    document.querySelector("#callusBtn").classList.toggle("come");
+}
+
+const callusBtn = document.querySelector("#callusBtn");
 const menuBtn = document.querySelector("#menuBtn");
+
 menuBtn.onclick = toggleMenu;
+callusBtn.onclick = toggleCall;
+
 
 /*------- LAST-MODIFIED --------*/
 

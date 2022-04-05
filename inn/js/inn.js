d4260da73d0f58912fbc3697158be84a9c88a370
rawinolivera <oli18001@byui.edu>
2022-04-05 12:45:36 -0500

    update responsive

--- inn/js/inn.js
+++ inn/js/inn.js
@@ -33,35 +33,6 @@ function showInfo() {
 
 const apiURL =
   "https://api.openweathermap.org/data/2.5/onecall?lat=-18.0227011&lon=-70.3175879&exclude=hourly&appid=0c8a48091627e17ece4ad85f202a2799&units=imperial";
-/*
-{"coord":
-    {"lon":-70.2483,
-    "lat":-18.0056},
-    "weather":[{"id":800,
-                "main":"Clear",
-                "description":"clear sky",
-                "icon":"01d"}],
-    "base":"stations",
-    "main":{"temp":66.4,
-            "feels_like":65.37,
-            "temp_min":63.5,
-            "temp_max":66.4,
-            "pressure":1014,
-            "humidity":56},
-    "visibility":10000,
-    "wind":{"speed":5.75,
-            "deg":230},
-    "clouds":{"all":0},
-    "dt":1649087566,
-    "sys":{"type":1,
-            "id":8701,
-            "country":"PE",
-            "sunrise":1649069266,
-            "sunset":1649111987},
-    "timezone":-18000,
-    "id":3928128,
-    "name":"Tacna","cod":200}
-*/
 
 fetch(apiURL)
   .then((response) => response.json())

f6bd0f0e65f3fc703469af04700baa4700f505a6
rawinolivera <oli18001@byui.edu>
2022-04-05 00:17:41 -0500

    update weather api

--- inn/js/inn.js
+++ inn/js/inn.js
@@ -68,7 +68,8 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
     document.querySelector("#temp").textContent = jsObject.current.temp; //temperature
-    document.querySelector("#condition").textContent = jsObject.current.weather[0].description; //condition
+    const desc = jsObject.current.weather[0].description;
+    document.querySelector("#condition").textContent = desc; //condition
     document.querySelector("#humidity").textContent = jsObject.current.humidity; //humidity
 
     const iconsrc = `https://openweathermap.org/img/w/${jsObject.current.weather[0].icon}.png`;
@@ -76,6 +77,81 @@ fetch(apiURL)
     document.querySelector("#current-icon").setAttribute("src", iconsrc);
     document.querySelector("#current-icon").setAttribute("alt", desc);
 
+    document.querySelector("#t1").textContent = jsObject.daily[1].temp.day;
+    document.querySelector("#t2").textContent = jsObject.daily[2].temp.day;
+    document.querySelector("#t3").textContent = jsObject.daily[3].temp.day;
     
+
+    function timeConverter(){
+
+      const dt = jsObject.current.dt;
+
+      var a = new Date(dt * 1000);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = date + ' ' + month + ' ' + year /*+ ' ' + hour + ':' + min + ':' + sec*/ ;
+      return time;
+    }
+
+    document.querySelector("#c-day").textContent = timeConverter(0);
+
+    function timeConverter1(){
+
+      const dt1 = jsObject.daily[1].dt;
+
+      var a = new Date(dt1 * 1000);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = date + ' ' + month + ' '/* + year + ' ' + hour + ':' + min + ':' + sec*/ ;
+      return time;
+    }
+
+    document.querySelector("#firstDay").textContent = timeConverter1(0);
+
+    function timeConverter2(){
+
+      const dt2 = jsObject.daily[2].dt;
+
+      var a = new Date(dt2 * 1000);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = date + ' ' + month + ' '/* + year + ' ' + hour + ':' + min + ':' + sec*/ ;
+      return time;
+    }
+
+    document.querySelector("#secondDay").textContent = timeConverter2(0);
+
+    function timeConverter3(){
+
+      const dt3 = jsObject.daily[3].dt;
+
+      var a = new Date(dt3 * 1000);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = date + ' ' + month + ' '/* + year + ' ' + hour + ':' + min + ':' + sec*/ ;
+      return time;
+    }
+
+    document.querySelector("#thirdDay").textContent = timeConverter3(0);
 
   });

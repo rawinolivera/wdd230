9a2b6a442ede101da0cdec11e4724cc9b84ff4f2
rawinolivera <oli18001@byui.edu>
2022-04-06 16:47:02 -0500

    update js

--- inn/js/temple.js
+++ inn/js/temple.js
@@ -29,3 +29,46 @@ document.querySelector('#last-mod').textContent = document.lastModified;
 function showInfo() {
     //document.querySelector()
 }
+
+
+/* ------ JSON DATA ------ */
+
+const requestURL = 'https://rawinolivera.github.io/wdd230/inn/data/temple.json';
+const chips = document.querySelector('.chips');
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const temples = jsonObject['temples'];
+        temples.forEach(displayTemples);
+    });
+
+function displayTemples(temple) {
+    let chip = document.createElement('section');
+    let templeName = document.createElement('h3');
+    let picture = document.createElement('img');
+    let address = document.createElement('p');
+    let telephone = document.createElement('p');
+    let email = document.createElement('p');
+
+    templeName.textContent = `${temple.name}`;
+    picture.src = temple.imageurl;
+    address.textContent = `${temple.address}`;
+    telephone.textContent = `${temple.telephone}`;
+    email.textContent = `${temple.email}`;
+    
+    picture.alt = `{temple.name}`;
+    chip.className = `unicard`;
+    templeName.className = `templeName`; 
+
+    chip.appendChild(templeName);
+    chip.appendChild(picture);
+    chip.appendChild(address);
+    chip.appendChild(telephone);
+    chip.appendChild(email);
+
+    chips.appendChild(chip);
+}

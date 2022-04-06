36cb76037db7c614ece3992420190b5d32e7c19f
rawinolivera <oli18001@byui.edu>
2022-04-06 16:04:50 -0500

    update menues

---
+++ inn/js/temple.js
@@ -0,0 +1,31 @@
+/*------- MENU ----------- */
+
+function toggleMenu() {
+    document.querySelector(".menu").classList.toggle("open");
+    document.querySelector("#menuBtn").classList.toggle("open");
+    document.querySelector("#banner").classList.toggle("open");
+}
+
+function toggleCall() {
+    document.querySelector("#phone").classList.toggle("come");
+    document.querySelector("#callusBtn").classList.toggle("come");
+}
+
+const callusBtn = document.querySelector("#callusBtn");
+const menuBtn = document.querySelector("#menuBtn");
+
+menuBtn.onclick = toggleMenu;
+callusBtn.onclick = toggleCall;
+
+
+/*------- LAST-MODIFIED --------*/
+
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector('#current-year').textContent = year;
+document.querySelector('#last-mod').textContent = document.lastModified;
+
+/*-------- PHONE CLICK -------*/
+function showInfo() {
+    //document.querySelector()
+}
